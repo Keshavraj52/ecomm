d11bd9e5a777c99922af5665f151821ebf982f18
Keshavraj_52 <poreg79@gmail.com>
2023-08-11 13:11:45 +0530

    seller 50% com

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -13,12 +13,7 @@ export class SellerAuthComponent implements OnInit {
   
   ngOnInit(): void {}
   signUp(data:signUp): void {
-   this.seller.userSignUp(data).subscribe((result)=>{
-   if(result){
-    this.router.navigate(['seller-home'])
-
-   }
-  });
+   this.seller.userSignUp(data)
   }
 
 }
--- src/app/services/seller.service.ts
+++ src/app/services/seller.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { signUp } from '../data-type';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class SellerService {
-
+  isSellerloggedIn= new BehaviorSubject<boolean>(false)
   constructor(private http:HttpClient) { }
   userSignUp(data:signUp){
-    return this.http.post('http://localhost:3000/seller',data)
+    this.http
+    .post('http://localhost:3000/seller',data,{observe:'response'})
+    .subscribe((result)=>{
+      this.isSellerloggedIn.next(true);
+      console.warn('result',result);
+
+    });
+    return false;
   }
 }
